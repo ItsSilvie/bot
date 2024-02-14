5200291adadde1266cfead08f7488e153816fc1f
James Donnelly <2203377+JamesDonnelly@users.noreply.github.com>
2024-02-14 11:32:47 +0000

    Update links.

--- dist/data/links.js
+++ dist/data/links.js
@@ -1,37 +1,46 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const links = [{
-        name: 'Home page',
+        name: 'Grand Archive website',
         url: 'https://gatcg.com',
     }, {
-        name: 'Index (card database)',
+        name: 'GA Index (card database)',
         url: 'https://index.gatcg.com',
     }, {
-        name: 'API docs',
+        name: 'GA Omnidex (organized play)',
+        url: 'https://omni.gatcg.com',
+    }, {
+        name: 'GA API docs',
         url: 'https://index.gatcg.com/api',
     }, {
-        name: 'Kickstarter campaign',
-        url: 'https://www.kickstarter.com/projects/weebs/grand-archive-tcg',
-    }, {
-        name: 'Grand Archive Discord',
+        name: 'GA Discord',
         url: 'https://discord.gg/nbgDk6UrFp',
     }, {
         name: 'Silvie.org Discord',
         url: 'https://discord.gg/bC6uWYvM4G',
     }, {
-        name: 'Facebook',
+        name: 'GA Facebook',
         url: 'https://www.facebook.com/grandarchivetcg',
     }, {
-        name: 'Instagram',
+        name: 'GA Instagram',
         url: 'https://www.instagram.com/grandarchivetcg',
     }, {
-        name: 'Twitter',
+        name: 'GA Twitter',
         url: 'https://twitter.com/GrandArchiveTCG',
     }, {
-        name: 'GA Marketplace',
+        name: 'Marketplace Discord',
         url: 'https://discord.gg/UQcCfrBFbd',
     }, {
-        name: 'Merlin\'s Marketplace',
+        name: 'Merlin\'s Marketplace Discord',
         url: 'https://discord.gg/bXV57zUcm4',
+    }, {
+        name: 'Silvie.org Deck Builder',
+        url: 'https://build-v2.silvie.org',
+    }, {
+        name: 'Shout at your Decks',
+        url: 'https://shoutatyourdecks.com',
+    }, {
+        name: 'Silvie.org Collection Tracker',
+        url: 'https://collect.silvie.org',
     }];
 exports.default = links;
--- src/data/links.ts
+++ src/data/links.ts
@@ -1,38 +1,47 @@
 import { Link } from "./types";
 
 const links = <Link[]>[{
-  name: 'Home page',
+  name: 'Grand Archive website',
   url: 'https://gatcg.com',
 }, {
-  name: 'Index (card database)',
+  name: 'GA Index (card database)',
   url: 'https://index.gatcg.com',
 }, {
-  name: 'API docs',
+  name: 'GA Omnidex (organized play)',
+  url: 'https://omni.gatcg.com',
+}, {
+  name: 'GA API docs',
   url: 'https://index.gatcg.com/api',
 }, {
-  name: 'Kickstarter campaign',
-  url: 'https://www.kickstarter.com/projects/weebs/grand-archive-tcg',
-}, {
-  name: 'Grand Archive Discord',
+  name: 'GA Discord',
   url: 'https://discord.gg/nbgDk6UrFp',
+}, {
+  name: 'GA Facebook',
+  url: 'https://www.facebook.com/grandarchivetcg',
+}, {
+  name: 'GA Instagram',
+  url: 'https://www.instagram.com/grandarchivetcg',
+}, {
+  name: 'GA Twitter',
+  url: 'https://twitter.com/GrandArchiveTCG',
+}, {
+  name: 'Marketplace Discord',
+  url: 'https://discord.gg/UQcCfrBFbd',
+}, {
+  name: 'Merlin\'s Marketplace Discord',
+  url: 'https://discord.gg/bXV57zUcm4',
+}, {
+  name: 'Silvie.org Deck Builder',
+  url: 'https://build-v2.silvie.org',
+}, {
+  name: 'Shout at your Decks',
+  url: 'https://shoutatyourdecks.com',
+}, {
+  name: 'Silvie.org Collection Tracker',
+  url: 'https://collect.silvie.org',
 }, {
   name: 'Silvie.org Discord',
   url: 'https://discord.gg/bC6uWYvM4G',
-}, {
-  name: 'Facebook',
-  url: 'https://www.facebook.com/grandarchivetcg',
-}, {
-  name: 'Instagram',
-  url: 'https://www.instagram.com/grandarchivetcg',
-}, {
-  name: 'Twitter',
-  url: 'https://twitter.com/GrandArchiveTCG',
-}, {
-  name: 'GA Marketplace',
-  url: 'https://discord.gg/UQcCfrBFbd',
-}, {
-  name: 'Merlin\'s Marketplace',
-  url: 'https://discord.gg/bXV57zUcm4',
 }];
 
 export default links;
